950c404be3a508c9a0d443be165e03fd7c15fe06
Antoine Aube <aaube3@gmail.com>
2024-01-04 11:14:37 -0700

    errorhandling: add error handling for create user

--- market_nextdoor_api/views.py
+++ market_nextdoor_api/views.py
@@ -28,6 +28,8 @@ def create_customer(request):
   if serializer.is_valid():
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+  else:
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def customer_details(request, customer_id):
