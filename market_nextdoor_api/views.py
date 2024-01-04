1c9b6cacf15b361a3a7bd9992f21b1c50a99bb44
Dylan Timmons <dylanstimmons@icloud.com>
2024-01-04 16:54:05 -0700

    Add Weather API

--- market_nextdoor_api/views.py
+++ market_nextdoor_api/views.py
@@ -5,6 +5,10 @@ from rest_framework import status
 from .serializers import *
 from .models import *
 import pdb
+import requests
+import os
+from dotenv import load_dotenv
+
 
 
 # Customer CRUD functions (SRP)
@@ -338,3 +342,29 @@ def update_preorder(preorder, data):
     preorder_data.save()
     return Response(preorder_data.data)
   return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# OpenWeather API
+@api_view(['GET'])
+def weather(request):
+    if request.method == 'GET':
+        zipcode = request.GET.get('zipcode', '80020')
+        return get_weather(request, zipcode)
+
+def get_weather(request, zipcode):
+    appid = '8fe63c807f5a5c8cce5e070949033a96'
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q': zipcode, 'appid': appid}
+
+    r = requests.get(url=URL, params=PARAMS)
+    
+    if r.status_code == 200:
+        res = r.json()
+        description = res['weather'][0]['description']
+        temp = round(res['main']['temp'] - 255.37)
+
+        return JsonResponse({'description': description, 'temp': temp})
+    else:
+        error_message = f"Error: {r.status_code}"
+        return JsonResponse({'error': error_message}, status=500)
+  
