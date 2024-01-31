3333eefaf67037b069be1910487f712a4581bd95
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-30 20:31:46 -0600

    Update the v2/admin.py file for updated naming accuracy

---
+++ v2/admin.py
@@ -0,0 +1,11 @@
+from .models import * 
+from django.contrib import admin
+
+admin.site.register(Market)
+admin.site.register(Vendor)
+admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Preorder)
+admin.site.register(PreorderItem)
+admin.site.register(VendorMarket)
+admin.site.register(CustomerMarket)
