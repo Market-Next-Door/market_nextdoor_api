9e4696bc65138ea403ef8d221e9c82d139f61b44
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-25 23:30:38 -0500

    Add the functionality to create, read, update, and destroy preorders based on a specific customer at a specific market

--- v2/views/customer_views.py
+++ v2/views/customer_views.py
@@ -2,8 +2,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from ..serializers import CustomerSerializer
-from ..models import Customer, Market
+from ..serializers import CustomerSerializer, PreorderSerializer
+from ..models import Customer, Market, Preorder, PreorderItem, Item
+import pdb
 
 
 @api_view(['GET', 'POST']) 
@@ -57,6 +58,7 @@ def delete_customer(customer):
   customer.delete()
   return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 # Customer by Market
 @api_view(['GET'])
 def customers_by_market_list(request, market_id):
@@ -85,7 +87,6 @@ def customer_by_market_details(request, customer_id, market_id):
     market = Market.objects.get(pk=market_id)
   except Market.DoesNotExist:
     return Response({"error": "Market not found."}, status=status.HTTP_404_NOT_FOUND)
-  
   try:
     customer = market.customers.get(pk=customer_id)
   except Customer.DoesNotExist:
@@ -93,3 +94,99 @@ def customer_by_market_details(request, customer_id, market_id):
 
   serializer = CustomerSerializer(customer)
   return Response(serializer.data)
+
+
+# Customer Preorders
+@api_view(['GET', 'POST'])
+def preorder_by_customer_list(request, market_id, customer_id):
+  # Input validation
+  if not (isinstance(market_id, int) and market_id > 0):
+    return Response({"error": "Invalid market id."}, status=status.HTTP_400_BAD_REQUEST)
+  if not (isinstance(customer_id, int) and customer_id > 0):
+    return Response({"error": "Invalid customer id."}, status=status.HTTP_400_BAD_REQUEST)
+  
+  try:
+    market = Market.objects.get(pk=market_id)
+  except Market.DoesNotExist:
+    return Response({"error": "Market not found."}, status=status.HTTP_404_NOT_FOUND)
+  try:
+    customer = market.customers.get(pk=customer_id)
+  except customer.DoesNotExist:
+    return Response({"error": "Customer not found."}, status=status.HTTP_404_NOT_FOUND)
+
+  if request.method == 'GET':
+    return get_preorder_list(request, customer)
+  elif request.method == 'POST':
+    return create_preorder(request, customer)
+
+def get_preorder_list(request, customer):
+  preorders = Preorder.objects.filter(customer=customer)
+  serializer = PreorderSerializer(preorders, many=True)
+  return Response(serializer.data)
+
+def create_preorder(request, customer):
+  serializer = PreorderSerializer(data=request.data)
+  if serializer.is_valid():
+    preorder = serializer.save(customer=customer)
+    response = preorder_item_helper(request, preorder, serializer)
+    return response
+  else:
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+def preorder_item_helper(request, preorder, serializer):
+  items = request.data.get('items', [])
+  for item_data in items:
+    try:
+      item = Item.objects.get(pk=item_data["item"])
+    except Item.DoesNotExist:
+      return Response({"error":f'Item {item_data["item"]} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+      
+    PreorderItem.objects.create(
+      preorder=preorder,
+      item_id=item.id,
+      quantity_requested=item_data['quantity']
+      )
+  return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def preorder_by_customer_details(request, market_id, customer_id, preorder_id):
+  # Input validation
+  if not (isinstance(market_id, int) and market_id > 0):
+    return Response({"error": "Invalid market id."}, status=status.HTTP_400_BAD_REQUEST)
+  if not (isinstance(customer_id, int) and customer_id > 0):
+    return Response({"error": "Invalid customer id."}, status=status.HTTP_400_BAD_REQUEST)
+  
+  try:
+    market = Market.objects.get(pk=market_id)
+  except Market.DoesNotExist:
+    return Response({"error": "Market not found."}, status=status.HTTP_404_NOT_FOUND)
+  try:
+    customer = market.customers.get(pk=customer_id)
+  except customer.DoesNotExist:
+    return Response({"error": "customer not found."}, status=status.HTTP_404_NOT_FOUND)
+  try:
+    preorder = Preorder.objects.get(pk=preorder_id, customer=customer)
+  except Preorder.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    return get_preorder_customer_list_details(preorder)
+  elif request.method == 'PUT':
+    return update_preorder(preorder, request.data)
+  elif request.method == 'DELETE':
+    return delete_preorder(preorder)
+
+def get_preorder_customer_list_details(preorder):
+  serializer = PreorderSerializer(preorder)
+  return Response(serializer.data)
+
+def update_preorder(preorder, data):
+  preorder_data = PreorderSerializer(preorder, data=data, partial=True)
+  if preorder_data.is_valid():
+    preorder_data.save()
+    return Response(preorder_data.data)
+  return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+def delete_preorder(preorder): 
+  preorder.delete()
+  return Response({'message': "deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
