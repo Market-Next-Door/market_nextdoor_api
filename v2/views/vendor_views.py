14c00616c361d234b6b61a1d5059fc0b65686b44
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-25 19:30:08 -0500

    Update the vendor_views file to account for the 'vendor by market' endpoints

--- v2/views/vendor_views.py
+++ v2/views/vendor_views.py
@@ -59,66 +59,41 @@ def delete_vendor(vendor):
   return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Vendor by Market
-@api_view(['GET', 'POST'])
-def vendor_by_market_list(request, market_id):
-  if request.method == 'GET':
-    return get_vendor_list(request)
-  elif request.method == 'POST':
-    return create_vendor(request)
+@api_view(['GET'])
+def vendors_by_market_list(request, market_id):
+  # Input validation
+  if not (isinstance(market_id, int) and market_id > 0):
+    return Response({"error": "Invalid market id."}, status=status.HTTP_400_BAD_REQUEST)
 
   try:
-    return Market.objects.get(pk=market_id)
+    market = Market.objects.get(pk=market_id)
   except Market.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-def get_vendor_by_market_list(request):
-  vendors = Vendor.objects.all()
+    return Response(stats=status.HTTP_404_NOT_FOUND)
+  
+  vendors = market.vendors.all()
   serializer = VendorSerializer(vendors, many=True)
   return Response(serializer.data)
 
-def create_vendor_by_market(request):
-  serializer = VendorSerializer(data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
 def vendor_by_market_details(request, vendor_id, market_id):
-  vendor = get_vendor_by_market_object(vendor_id)
-  
-  if request.method == 'GET':
-    return get_vendor_by_market_details(vendor)
-  elif request.method == 'PUT':
-    return update_vendor_by_market(vendor, request.data)
-  elif request.method == 'DELETE':
-    return delete_vendor_by_market(vendor)
+  # Input validation
+  if not (isinstance(market_id, int) and market_id > 0):
+    return Response({"error": "Invalid market id."}, status=status.HTTP_400_BAD_REQUEST)
+  if not (isinstance(vendor_id, int) and vendor_id > 0):
+    return Response({"error": "Invalid vendor id."}, status=status.HTTP_400_BAD_REQUEST)
 
   try:
-    return Market.objects.get(pk=market_id)
+    market = Market.objects.get(pk=market_id)
   except Market.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response({"error": "Market not found."}, status=status.HTTP_404_NOT_FOUND)
   
-def get_vendor_by_market_object(market_id, vendor_id):
   try:
-    return Vendor.objects.get(pk=vendor_id)
+    vendor = market.vendors.get(pk=vendor_id)
   except Vendor.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response({"error": "Vendor not found."}, status=status.HTTP_404_NOT_FOUND)
 
-def get_vendor_by_market_details(vendor):
   serializer = VendorSerializer(vendor)
   return Response(serializer.data)
-
-def update_vendor_by_market(vendor, data):
-  vendor_data = VendorSerializer(vendor, data=data, partial=True)
-  if vendor_data.is_valid():
-    vendor_data.save()
-    return Response(vendor_data.data)
-  return Response(vendor_data.errors, status=status.HTTP_400_BAD_REQUEST)
-
-def delete_vendor_by_market(vendor):
-  vendor.delete()
-  return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # Preorder CRUD functions - Vendor list (SRP)
