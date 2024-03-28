7dc02e5c70f7028fce94202fb4fc3f7778ef289d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-28 15:22:29 -0500

    Rename most of the url names so that the v2 view test suite can find the urls

--- v2/urls.py
+++ v2/urls.py
@@ -28,27 +28,27 @@ urlpatterns = [
     
     # V2 Endpoints
     # markets
-    path('markets/', market_views.market_list, name='test_name'),
-    path('markets/<int:market_id>/', market_views.market_details, name='test_name'),
+    path('markets/', market_views.market_list, name='market_list_v2'),
+    path('markets/<int:market_id>/', market_views.market_details, name='market_details_v2'),
     # vendors
-    path('vendors/', vendor_views.vendor_list, name='test_name'),
-    path('vendors/<int:vendor_id>/', vendor_views.vendor_details, name='test_name'),
+    path('vendors/', vendor_views.vendor_list, name='full_vendor_list_v2'),
+    path('vendors/<int:vendor_id>/', vendor_views.vendor_details, name='full_vendor_details_v2'),
     ## vendors by market
     path('markets/<int:market_id>/vendors/', vendor_views.vendors_by_market_list, name='test_name'),
     path('markets/<int:market_id>/vendors/<int:vendor_id>/', vendor_views.vendor_by_market_details, name='test_name'),
     # customers
-    path('customers/', customer_views.customer_list, name='test_name'),
-    path('customers/<int:customer_id>/', customer_views.customer_details, name='test_name'),
+    path('customers/', customer_views.customer_list, name='full_customer_list_v2'),
+    path('customers/<int:customer_id>/', customer_views.customer_details, name='full_customer_details_v2'),
     ## customers by market
     path('markets/<int:market_id>/customers/', customer_views.customers_by_market_list, name='test_name'),
     path('markets/<int:market_id>/customers/<int:customer_id>/', customer_views.customer_by_market_details, name='test_name'),
     # items
-    path('vendors/<int:vendor_id>/items/', item_views.item_list, name='test_name'),
-    path('vendors/<int:vendor_id>/items/<int:item_id>/', item_views.item_details, name='test_name'),
+    path('vendors/<int:vendor_id>/items/', item_views.item_list, name='item_list_v2'),
+    path('vendors/<int:vendor_id>/items/<int:item_id>/', item_views.item_details, name='item_details_v2'),
     # customer's preorders
-    path('markets/<int:market_id>/customers/<int:customer_id>/preorders/', customer_views.preorder_by_customer_list, name='test_name'),
-    path('markets/<int:market_id>/customers/<int:customer_id>/preorders/<int:preorder_id>/', customer_views.preorder_by_customer_details, name='test_name'),
+    path('markets/<int:market_id>/customers/<int:customer_id>/preorders/', customer_views.preorder_by_customer_list, name='customer_preorder_list_v2'),
+    path('markets/<int:market_id>/customers/<int:customer_id>/preorders/<int:preorder_id>/', customer_views.preorder_by_customer_details, name='customer_preorder_details_v2'),
     # vendor's preorders
-    path('markets/<int:market_id>/vendors/<int:vendor_id>/preorders/', vendor_views.preorder_by_vendor_list, name='test_name'),
-    path('markets/<int:market_id>/vendors/<int:vendor_id>/preorders/<int:preorder_id>/', vendor_views.preorder_by_vendor_details, name='test_name')
+    path('markets/<int:market_id>/vendors/<int:vendor_id>/preorders/', vendor_views.preorder_by_vendor_list, name='vendor_preorder_list_v2'),
+    path('markets/<int:market_id>/vendors/<int:vendor_id>/preorders/<int:preorder_id>/', vendor_views.preorder_by_vendor_details, name='vendor_preorder_details_v2')
 ]
