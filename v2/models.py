ff685822f0c456a4d0e8b28372c17ce79d52f1d1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-04-11 20:51:28 -0500

    Remove extra commented out code

--- v2/models.py
+++ v2/models.py
@@ -62,7 +62,7 @@ class Preorder(models.Model):
 class Market(models.Model):
     market_name = models.CharField(max_length=50, null=False)
     location = models.CharField(max_length =100, null=True)
-    listing_id = models.CharField(max_length=50, null=False) # This will need to be added to the database for when we get the map component plugged in. 
+    listing_id = models.CharField(max_length=50, null=False)
     details = models.CharField(max_length=100)
     start_date = models.DateField(null=True)
     end_date = models.DateField(null=True)
